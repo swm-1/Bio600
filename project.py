3ce65c14be8aada829a4059ac1eec6881e26360a
Sam Arnell <bt23208@qmul.ac.uk>
2025-09-23 15:42:27 +0100

    first inclusion of the gamma calculation

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy import constants
 import matplotlib.pyplot as plt
 import csv
-
+import math
 
 def beta_from_T(T_K: float) -> float:
     """Return beta = 1/(k_B T) in 1/eV."""
@@ -21,7 +21,91 @@ def rate_from_deltaG(delta_G_eV: float, beta: float, k_h: float) -> float:
     """
     return k_h if delta_G_eV <= 0.0 else (k_h * np.exp(-beta * delta_G_eV))
 
+def FWHM() -> float:
+    """
+    Calculate the full width half max value. The reason for doing this is so that
+    we can calculate w (or the standard deviation in for A_p(lambda))
 
+    the file named T01.absorption_Qyonly.txt is data for the Qy absorption band of chlorophyll A.
+    """
+    x, y = np.loadtxt("T01.absorption_Qyonly.txt", skiprows=1,unpack=True)
+
+    peak = np.argmax(y) # This is for indexing
+    # the interpolate function is going to look like this:
+    # np.interp(new_y, y, x)
+    new_y = np.max(y) / 2 # half max
+    left = np.interp(new_y, y[:peak+1], x[:peak+1]) # interpolation
+    right = np.interp(new_y, y[peak:][::-1], x[peak:][::-1]) # interpolation
+
+    fwhm = right - left
+    return fwhm
+
+def A_calc() -> np.array:
+    """
+    Small function that calculates A_p(lambda). See below for the equation (maybe at later date include 
+    latex code for the formula)
+    """
+    fwhm = FWHM()
+    x, y = np.loadtxt("T01.absorption_Qyonly.txt", skiprows=1,unpack=True)
+    w = fwhm / 2.35482
+    peak_y = np.argmax(y)
+    peak = x[peak_y]
+    list_A_vals = []
+
+    for wavelengths in x: # not sure how good it is to use the loop when dealing with vectorized quantities but it works
+                          # for now
+        first_part = 1 / (w * math.sqrt(2*math.pi))
+        exponetial = np.exp(-(wavelengths - peak)**2 / (2*w**2))
+        A = first_part * exponetial
+        list_A_vals.append(A)
+
+    array_A = np.asarray(list_A_vals)
+    return array_A   
+
+
+def check(A:np.array) -> None:
+
+    """
+    A_p(lambda) must integrate to have an overall area with 1. A_p(lambda) is dimensionless because it is expressed in nm^-1
+    and therefore when we integrate it is being multiplied by d lambda and thus the units cancel.
+    However this function is essentially doing nothing at the moment, i am not even sure if it checks the validity 
+    in the correct manner.
+    """
+
+    x, _ = np.loadtxt("T01.absorption_Qyonly.txt",skiprows=1, unpack=True)
+    integrated_A = np.trapezoid(A, x)
+
+    if np.isclose(integrated_A, 1.0 , rtol=1e-2) == True:
+        print("This is normalized and ready to use")
+    else:
+        raise ValueError("The value for A when integrated is not 1")
+    
+
+def Gamma(A, h=constants.h, c=constants.c, N=100, sigma=1e-20) -> float: 
+    
+   """
+   Units:
+   
+    - cross section (chlorophyll) = 10^-20 m^2
+    - spectral irradience = Wm^-2 nm^-1
+    - A_p(lambda) = dimensionless
+
+   """ 
+   check(A)
+   lam_pig, _ = np.loadtxt("T01.absorption_Qyonly.txt",unpack=True, skiprows=1)
+   lam_star, f = np.loadtxt("5800K.txt", unpack=True)
+
+   lam_pig = np.asarray(lam_pig, dtype=float).ravel()
+   lam_star = np.asarray(lam_star, dtype=float).ravel()
+   f = np.asarray(f, dtype=float).ravel()
+   
+   lam_star_m = lam_star * 1e-9
+   A_star = np.interp(lam_star,lam_pig,A)
+   photon_eq = lam_star_m / (h*c)
+   integrand = photon_eq * f * A_star
+   photons = np.trapezoid(integrand, lam_star)
+   gamma = N * sigma * photons
+   return gamma
 
 def adjacency_matrix(edges_csv: str, T_K: float = 300.0, tau_ps: float = 10.0):
     """
@@ -31,6 +115,8 @@ def adjacency_matrix(edges_csv: str, T_K: float = 300.0, tau_ps: float = 10.0):
     """
     beta = beta_from_T(T_K)
     k_h = 1.0 / tau_ps  # ps^-1
+    gamma_calc = Gamma(A_calc(),h=constants.h, c=constants.c, N=100, sigma=1e-20)
+    gamma_ps = gamma_calc / 1e12 # convert from seconds into picoseconds
 
     edges = []          # list of (i, j, rate)
     nodes = -1 # could be any negative number
@@ -39,19 +125,22 @@ def adjacency_matrix(edges_csv: str, T_K: float = 300.0, tau_ps: float = 10.0):
         reader = csv.DictReader(f)
         required = {"donor", "acceptor", "delta_G_eV", "rate"}
         if not reader.fieldnames or not required.issubset(set(reader.fieldnames)):
-            raise ValueError("CSV must have columns: donor, acceptor, delta_G_eV, rate")
+            raise ValueError("CSV must have columns: donor, acceptor, delta_G_eV, rate, gamma")
         for row in reader:
             i = int((row["donor"]).strip())
             j = int((row["acceptor"]).strip())
             raw_rate = (row.get("rate") or "").strip()
             raw_dG   = (row.get("delta_G_eV") or "").strip()
+            gamma    = (row.get("gamma") or "").strip()
 
             if raw_rate != "":
                 r = float(raw_rate)
             elif raw_dG != "":
                 r = rate_from_deltaG(float(raw_dG), beta, k_h)
+            elif gamma != "":
+                r = gamma_ps
             else:
-                raise ValueError("Each row needs either rate or delta_G_eV")
+                raise ValueError("Each row needs either rate or delta_G_eV or gamma value")
 
             if i == j:
                 raise ValueError(f"Self-loop not allowed at node {i}")
@@ -136,7 +225,7 @@ def main():
      The function then constructs P0 and creates the timestamps, then calls the relevant functions
 
     """
-    edges_path = "edges.csv"   # change this line if your CSV is named differently
+    edges_path = "edges.csv"   # This line depends on the name of CSV file -> would CLI path be better?
 
     # Build A, K
     A, labels = adjacency_matrix(edges_path, T_K=300.0, tau_ps=10.0)
